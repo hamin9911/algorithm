6d030bc732f91e325b7c19eea39c36f70b4c989a
hamin9911 <tjddl2071@gmail.com>
2025-04-02 16:57:53 +0900

    Factorial 문제 추가

--- Beakjoon/Factorial.java
+++ Beakjoon/Factorial.java
@@ -2,7 +2,7 @@ package Beakjoon;
 
 import java.util.Scanner;
 
-public class Factoreal {
+public class Factorial {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
